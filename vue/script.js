7bc6c54f0a98cfa60a9085426e10503f102f9d6c
Antonio Piras <antonio.piras90@gmail.com>
2023-07-21 16:11:05 +0200

    html/css base e stampa tasks

---
+++ vue/script.js
@@ -0,0 +1,36 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            tasks: [
+                {
+                    id: 1,
+                    text: "to-do esempio 1",
+                    done: true
+                }, {
+                    id: 2,
+                    text: "to-do esempio 2",
+                    done: true
+                }, {
+                    id: 3,
+                    text: "to-do esempio 3",
+                    done:  true
+                }, {
+                    id: 4,
+                    text: "to-do esempio 4",
+                    done: true
+                }, {
+                    id: 5,
+                    text: "to-do esempio 5",
+                    done: true
+                }
+            ],
+            
+        };
+
+    },
+    methods: {
+       
+    },
+});
+console.log(app);
+app.mount("#app")
