073c959f1ac12cc41a6dd0529cca3dd21a260445
Antonio Piras <antonio.piras90@gmail.com>
2023-07-21 17:35:50 +0200

    cliccando sulla x la task viene rimossa

--- vue/script.js
+++ vue/script.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
+
             tasks: [
+
                 {
                     id: 1,
                     text: "to-do esempio 1",
@@ -13,7 +15,7 @@ const app = Vue.createApp({
                 }, {
                     id: 3,
                     text: "to-do esempio 3",
-                    done:  true
+                    done: true
                 }, {
                     id: 4,
                     text: "to-do esempio 4",
@@ -24,12 +26,23 @@ const app = Vue.createApp({
                     done: true
                 }
             ],
-            
+
         };
 
     },
     methods: {
-       
+
+        // funzione al click per cancellare una task alla volta 
+        taskDelete(taskId) {
+
+            //  dichiaro la variabile indexToDElete con valore la funzione findIndex per recuperare l'elemento corrispondenrte alla condizione che andiamo a scrivere 
+            let indexToDElete = this.tasks.findIndex((tasksItem) => tasksItem.id === taskId);
+            //   console.log(indexToDElete);
+
+            // utilizzo splice per cancellare la task corrispondente all' indice della task cliccata(il numero sta per la quantità di elementi da cancellare)
+            this.tasks.splice(indexToDElete, 1)
+        },
+
     },
 });
 console.log(app);
